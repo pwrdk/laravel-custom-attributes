9915f4dcd2a1af7733de3a2edd57ade8048028dd
Peter Waerum <peter@datainsights.dk>
2019-11-29 17:38:22 +0100

    Added keys relationship to type model

--- src/Models/AttributeType.php
+++ src/Models/AttributeType.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class AttributeType extends Model
 {
     protected $guarded = [];
+
+
+    public function keys()
+    {
+        return $this->hasMany(AttributeKey::class, 'type_id');
+    }
 }
