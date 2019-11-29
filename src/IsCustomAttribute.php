f81e296f0ff6864c736bccf4a2ffab251129dbe9
Peter Waerum <peter@datainsights.dk>
2019-11-29 17:38:29 +0100

    Updated trait

--- src/IsCustomAttribute.php
+++ src/IsCustomAttribute.php
@@ -4,10 +4,20 @@ namespace PWRDK\CustomAttributes;
 
 trait IsCustomAttribute
 {
+    public function ownerAttribute()
+    {
+        return $this->belongsTo(CustomAttribute::class);
+    }
+
     public function getFields()
     {
         return collect($this->getAttributes())->filter(function ($val, $key) {
             return !in_array($key, $this->hidden);
-        })->toArray();
+        });
+    }
+
+    public function getAttributeValuesAttribute()
+    {
+        return $this->getFields()->values();
     }
 }
